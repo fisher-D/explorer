aaae7200930b427baa37a6d622289bf333b23057
Jack Dai <1594250583@qq.com>
2019-09-19 18:47:57 +0800

    removeInfo

--- pkgs/block.go
+++ pkgs/block.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	URL       = "http://admin:admin@47.244.98.227:13143"
+	URL       = "EXAMPLE"
 	GenesisTx = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
 	mongourl  = "localhost:27017"
 )
