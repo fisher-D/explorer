5d9d449ca5042a52377b762c0700346237836e61
Jack Dai <1594250583@qq.com>
2019-09-24 11:37:26 +0800

    remove-info

--- service/config.go
+++ service/config.go
@@ -1,9 +1,9 @@
 package service
 
 const (
-	BTCURL       = "http://admin:admin@47.244.98.227:13143"
-	BTCGenesisTx = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
-	LTCURL       = "http://test:test@47.244.98.227:13145"
-	LTCGenesisTx = "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"
-	Mongourl     = "localhost:27017"
+	BTCURL       = "example"
+	BTCGenesisTx = "example"
+	LTCURL       = "example"
+	LTCGenesisTx = "example"
+	Mongourl     = "example"
 )
